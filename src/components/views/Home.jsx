989e13fa132e84229794fa2454c3b22b57803b79
edmundmarievaldoz <edmundmarievaldoz@gmail.com>
2026-07-06 23:39:44 +0100

    added alt

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -30,14 +30,18 @@ function Home () {
                 <div className="feature-item">
                     <h1 className='items'>Near-Earth Objects</h1>
                     <p className='desc'>Real-time tracking of asteroid velocities, diameters, and close-approach proximity trajectories.</p>
-                    <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fd2pn8kiwq2w21t.cloudfront.net%2Foriginal_images%2F1_overview-eyes-on-asteroids.png&f=1&nofb=1&ipt=a2d9420399bd2d53959d163bcda6313160fcfdfae8573121e60fa337e0472734' className='nearObj'/>
+                    <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fd2pn8kiwq2w21t.cloudfront.net%2Foriginal_images%2F1_overview-eyes-on-asteroids.png&f=1&nofb=1&ipt=a2d9420399bd2d53959d163bcda6313160fcfdfae8573121e60fa337e0472734' 
+                    alt="space galaxy"
+                    className='nearObj'/>
                 </div>
                 
                 <div className="feature-item">
 
                     <h1 className='items'>Magnetosphere Weather</h1>
                     <p className='desc'>Live monitoring of solar flare disruptions, coronal mass ejections, and geomagnetic storms.</p>
-                    <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffacts.net%2Fwp-content%2Fuploads%2F2025%2F02%2F28-facts-about-magnetosphere-1738626093.jpg&f=1&nofb=1&ipt=fd745fa9d53e1deebd074bc380e8d3b342a855154df67bb3b6696e080b39a38e' className='nearObj'/>
+                    <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffacts.net%2Fwp-content%2Fuploads%2F2025%2F02%2F28-facts-about-magnetosphere-1738626093.jpg&f=1&nofb=1&ipt=fd745fa9d53e1deebd074bc380e8d3b342a855154df67bb3b6696e080b39a38e' 
+                    alt="Magnetosphere Weather"
+                    className='nearObj'/>
                 
                 </div>
 
