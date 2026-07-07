4c288281bb25adf2f0c090470136e76d11fbdd9b
edmundmarievaldoz <edmundmarievaldoz@gmail.com>
2026-07-07 18:17:09 +0100

    APOD scss implemented

--- src/components/views/APOD.jsx
+++ src/components/views/APOD.jsx
@@ -21,14 +21,13 @@ function APOD () {
         <p>{loadingApodMessage}</p>
       ) : (
         <div className="apod-container">
-          <h2>{apod.title}</h2>
-          
+
           {apod.media_type === 'video' ? (
             <iframe src={apod.url} title={apod.title} className="apod-video" />
           ) : (
             <img src={apod.url} alt={apod.title} className="apod-img" />
           )}
-
+          <h2>{apod.title}</h2>
           <p className="apod-explanation">{apod.explanation}</p>
         </div>
       )}
